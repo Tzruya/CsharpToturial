aa8470101e0e56f7347d5c16c89035487bccb253
Tzruya <109286665+Tzruya@users.noreply.github.com>
2023-04-27 14:35:29 +0300

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -1,1 +1,33 @@
+// כתוב פונקציה הקולטת מהמקלדת מטריצה בגודל 3 על 3 מסוג מספרים שלמים. הפונקציה תדפיס כמה ערכים במטריצה גדולים מ-10 וקטנים מ-100
+
+using System;
+
+namespace matrix
+{
+    class Program
+    {
+        static void f1(int[,] num)
+        {
+            int i, j;
+            int count = 0;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (num[i, j] > 10 && num[i, j] < 100)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+  }
+        static void Main(string[] args)
+        {
+            int[,] num = { {12,31,50},{30,42,41},{89,32,16}};
+            f1(num);
+        }
+    }
+}
+
 
